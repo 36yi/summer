98bd31eb218d529a6d89680dc7d8e21d2d542ab1
36yi <36yunjae@gmail.com>
2024-07-30 13:47:34 +0900

    work

--- src/main/java/com/ll/sapp/SappApplication.java
+++ src/main/java/com/ll/sapp/SappApplication.java
@@ -3,6 +3,7 @@ package com.ll.sapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SappApplication {
 
